7b65c5b4ea77909b0cd0b538ead1661ea3a9cd55
admin <sketchover@hotmail.com>
2017-08-11 15:13:23 +0700

    Update code syntax of IK_server.py

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -35,29 +35,32 @@ def handle_calculate_IK(req):
             # 1Start.Define DH param symbols
             d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')  # link offset
             a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')  # link length
-            alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 =  symbols('alpha0:7') # twist angle            
+            alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = \
+                symbols('alpha0:7')  # twist angle
             # 1End.Define DH param symbols
 
             # 2Start.Joint angle symbols
-            q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8') # theta
+            q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')  # theta
             # 2End.Joint angle symbols
 
             # 3Start.Modified DH params
-            DH_Table = { alpha0: 0,     a0:     0,      d1:     0.75,   q1: q1,
-                         alpha1: -pi/2., a1:	0.35,   d2:     0,  q2:	-pi/2. +q2,
-                         alpha2: 0,     a2:	1.25,   d3:     0,  q3:	q3,
-                         alpha3: -pi/2., a3:	-0.054, d4:     1.5,    q4:	q4,
-                         alpha4: pi/2., a4:	0,      d5:     0,  q5:	q5,
-                         alpha5: -pi/2., a5:	0,      d6:     0,  q6:	q6,
-                         alpha6: 0,     a6:	0,      d7:     0.303,  q7:	0} #            
+            DH_Table = {
+                alpha0: 0,     a0:     0,      d1:     0.75,   q1: q1,
+                alpha1: -pi/2., a1:    0.35,   d2: 0, q2: -pi/2.+q2,
+                alpha2: 0,     a2:	1.25,   d3:     0,  q3:	q3,
+                alpha3: -pi/2., a3: -0.054, d4: 1.5,    q4: q4,
+                alpha4: pi/2., a4:	0,      d5:     0,  q5:	q5,
+                alpha5: -pi/2., a5:	0,      d6:     0,  q6:	q6,
+                alpha6: 0,     a6:	0,      d7:     0.303,  q7:	0}
             # 3End.Modified DH params
 
             # 4Start.Define Modified DH Transformation matrix
             def TF_Matrix(alpha, a, d, q):
-                TF = Matrix([[	cos(q),		-sin(q),		0,		a],
-                             [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-                             [sin(q)*sin(alpha), cos(q)*sin(alpha), cos(alpha), cos(alpha)*d],
-                             [	0,		0,		0,		1]])
+                TF = Matrix([
+                    [	cos(q),		-sin(q),		0,		a],
+                    [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],  # noqa
+                    [sin(q)*sin(alpha), cos(q)*sin(alpha), cos(alpha), cos(alpha)*d],  # noqa
+                    [	0,		0,		0,		1]])
                 return TF
             # 4End.Define Modified DH Transformation matrix
 
@@ -73,10 +76,9 @@ def handle_calculate_IK(req):
 
             # transformation matrix : base_link ~ end effector
             # T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
-            
             # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+        # px,py,pz = end-effector position
+        # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
@@ -85,8 +87,7 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
 
-            ## Find EE rotation matrix
-            
+            # Find EE rotation matrix
             # Define RPY rotation matrices
             # http://planning.cs.uiuc.edu/node102.html
             r, p, y = symbols('r p y')
@@ -103,51 +104,68 @@ def handle_calculate_IK(req):
 
             # More information can be found in KR210 Forward Kinematics section
             # Rotation Error Correction
-            Rot_Error = ROT_z.subs( y, radians(180)) * ROT_y.subs(p, radians(-90))
+            Rot_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))  # noqa
             ROT_EE = ROT_EE * Rot_Error
             ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
             EE = Matrix([[px],
                          [py],
                          [pz]])
-            # wrist center : 
-            WC = EE - (0.303) * ROT_EE[:,2]
-            
+            # wrist center :
+            WC = EE - (0.303) * ROT_EE[:, 2]
+
             # 6S. Calculate joint angles using Geometric IK method
 
-            # More information can be found in the inverse kenematics with Kuka KR210
+            # More information can be found in the inverse kenematics
+            # with Kuka KR210
             theta1 = atan2(WC[1], WC[0])
-            
+
             # SSS triangle for theta2 and theta3
             side_a = 1.501
-            side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
+            side_b = sqrt(
+                    pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) +
+                    pow((WC[2] - 0.75), 2))
             side_c = 1.25
 
-            angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
-            angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
-            angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
+            angle_a = acos(
+                (side_b * side_b + side_c * side_c - side_a * side_a) /
+                (2 * side_b * side_c))
+            angle_b = acos(
+                (side_a * side_a + side_c * side_c - side_b * side_b) /
+                (2 * side_a * side_c))
+            angle_c = acos(
+                (side_a * side_a + side_b * side_b - side_c * side_c) /
+                (2 * side_a * side_b))
 
-            theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
-            theta3 = pi / 2 - (angle_b + 0.036)  # 0.036 accounts for sag in link4 of -0.054m
+            theta2 = pi / 2 - angle_a - atan2(
+                    WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+            # 0.036 accounts for sag in link4 of -0.054m
+            theta3 = pi / 2 - (angle_b + 0.036)
 
             # R0_3 = T0_1, T1_2, T2_3
             R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
             R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
             R3_6 = R0_3.inv("LU") * ROT_EE
-            
+
             # Euler angles from rotation matrix
-            # More information can be found in the Euler Angles from a Rotaion Matrix section
-            theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-            theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
-            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
-            
+            # More information can be found in the Euler Angles
+            # from a Rotaion Matrix section
+            theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
+            theta5 = atan2(
+                sqrt(R3_6[0, 2] * R3_6[0, 2] + R3_6[2, 2] * R3_6[2, 2]),
+                R3_6[1, 2])
+            theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
+
             # 6End. Calculate joint angles using Geometric IK method
 
             # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+            # In the next line replace theta1,theta2...,theta6
+            # by your joint angle variables
+        joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]  # noqa
+        joint_trajectory_list.append(joint_trajectory_point)
 
-        rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+        rospy.loginfo(
+            "length of Joint Trajectory List: %s" %
+            len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
 
 
